f32ce12ae7b0893ac77f5b87cbd11de64330724d
Diogo Vianna V. Araujo <diogovaraujo@gmail.com>
2022-06-02 23:20:38 -0300

    Added what is link.

--- components/Footer.js
+++ components/Footer.js
@@ -8,9 +8,10 @@ export const Footer = () => {
   const languages = page ? [page.lang, ...page.alternate_languages] : [];
 
   return (
-    <footer className="flex flex-wrap items-center justify-between gap-x-6 gap-y-3 leading-none py-8">
+    <footer className="flex py-8 font-bold uppercase">
+      <div className="flex w-1/2 justify-center">o que é</div>
       <nav className="flex w-1/2 justify-center">
-        <ul className="flex flex-wrap gap-6 font-bold uppercase md:gap-10">
+        <ul className="flex flex-wrap gap-6 md:gap-10">
           {languages.map((lang) => {
             return lang.lang ? (
               <li key={lang.lang}>
