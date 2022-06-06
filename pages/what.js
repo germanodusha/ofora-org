c7ea7b8a6348e6316045441ae7fbecf8b4afc52b
Diogo Vianna V. Araujo <diogovaraujo@gmail.com>
2022-06-05 23:20:54 -0300

    Added basic about page.

--- pages/what.js
+++ pages/what.js
@@ -1,11 +1,37 @@
-// a page rendering the content of cms about document
+import { useContext, useEffect } from "react";
+import { Context } from "../pages/_app";
+import { createClient } from "../prismicio";
+import { PrismicText } from "@prismicio/react";
 
-const About = ({ data }) => {
+const About = ({ page }) => {
+    const context = useContext(Context);
+
+    useEffect(() => {
+      context.setPage(page);
+    }, []);
+
   return (
     <>
-      oi
+    <video autoPlay loop muted playsInline>
+        <source src={page.data.video.url} type="video/mp4" />
+    </video>
+    <h1>{page.data.title}</h1>
+    <PrismicText field={page.data.content} />
     </>
   );
 };
+
+export async function getStaticProps({ locale, previewData }) {
+    const client = createClient({ previewData });
+  
+    const page = await client.getSingle("what", { lang: locale });
+  
+    return {
+      props: {
+        page
+      },
+    };
+  }
+  
 
 export default About;
