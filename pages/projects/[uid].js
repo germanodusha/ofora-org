c6642dff0a833efc072c90e8836f3e21be47a19c
Diogo Vianna V. Araujo <diogovaraujo@gmail.com>
2022-06-24 18:48:43 -0300

    Project page limiting.

--- pages/projects/[uid].js
+++ pages/projects/[uid].js
@@ -2,64 +2,67 @@ import { PrismicRichText } from "@prismicio/react";
 import Image from "next/image";
 import { createClient } from "../../prismicio";
 import { Logo } from "../../components/Logo";
+import { Limiter } from "../../components/Limiter";
 
 const Project = ({ project }) => {
   const { cover, banner } = project.data;
   const image = banner.url ? banner : cover;
   return (
     <div className="page-root">
-      <div className="background" />
-      <div className="flex h-screen grow items-center justify-center">
-        <div className="banner">
-          {image.url && (
-            <Image
-              src={image.url}
-              width={image.dimensions.width}
-              height={image.dimensions.height}
-              alt={image.alt}
-              layout="fill"
-              objectFit="contain"
-            />
-          )}
-        </div>
-        <div className="title text-center uppercase">
-          <h1 className="text-8xl">{project.data.title}</h1>
-          <h2 className="text-8xl">
-            {project.data.year}
-            <br />
-          </h2>
-          <div className="pt-5 text-xl">{project.data.category}</div>
-        </div>
-        <div className="logo">
-          <Logo />
-        </div>
-      </div>
-      <div className="intro p-20 text-center text-3xl">
-        <PrismicRichText field={project.data.intro} />
-      </div>
-      <div className="gallery p-20">
-        {project.data.gallery.map((item) => (
-          <div className="item" key={item.url}>
-            {item.thumb.kind === "image" ? (
+      <Limiter>
+        <div className="background" />
+        <div className="flex h-screen grow items-center justify-center">
+          <div className="banner">
+            {image.url && (
               <Image
-                key={item.thumb.url}
-                src={item.thumb.url}
-                width={item.thumb.width/item.thumb.height*250}
-                height={250}
-                alt={item.thumb.alt}
+                src={image.url}
+                width={image.dimensions.width}
+                height={image.dimensions.height}
+                alt={image.alt}
+                layout="fill"
+                objectFit="contain"
               />
-            ) : (
-              <video autoPlay playsInline muted>
-                <source src={item.thumb.url} type="video/mp4" />
-              </video>
             )}
-            <span>{item.title}</span>
           </div>
-        ))}
-      </div>
-      <div className="content columns-2 p-20">
-        <PrismicRichText field={project.data.content} />
-      </div>
+          <div className="title text-center uppercase">
+            <h1 className="text-8xl">{project.data.title}</h1>
+            <h2 className="text-8xl">
+              {project.data.year}
+              <br />
+            </h2>
+            <div className="pt-5 text-xl">{project.data.category}</div>
+          </div>
+          <div className="logo">
+            <Logo />
+          </div>
+        </div>
+        <div className="intro p-10 md:p-20 text-center md:text-xl lg:text-3xl">
+          <PrismicRichText field={project.data.intro} />
+        </div>
+        <div className="gallery p-20">
+          {project.data.gallery.map((item) => (
+            <div className="item" key={item.url}>
+              {item.thumb.kind === "image" ? (
+                <Image
+                  key={item.thumb.url}
+                  src={item.thumb.url}
+                  width={(item.thumb.width / item.thumb.height) * 320}
+                  height={320}
+                  alt={item.thumb.alt}
+                />
+              ) : (
+                <video autoPlay playsInline muted>
+                  <source src={item.thumb.url} type="video/mp4" />
+                </video>
+              )}
+              <span>{item.title}</span>
+            </div>
+          ))}
+        </div>
+        <div className="content columns-2 p-20">
+          <PrismicRichText field={project.data.content} />
+        </div>
+      </Limiter>
       <style jsx>{`
         .background {
           box-shadow: inset 0px 0px 120px 150px rgba(152, 152, 152, 1);
@@ -128,21 +131,26 @@ const Project = ({ project }) => {
         .item :global(video):hover {
           opacity: 0.4;
         }
-        @media only screen and (min-width: 800px) {
+        @media only screen and (min-width: 780px) {
           .gallery {
             display: flex;
             flex-wrap: wrap;
-            justify-content: center;
+            justify-content: space-between;
           }
           .item :global(img),
           .item :global(video) {
-            max-height: 160px;
+            max-height: 100px;
           }
         }
-        @media only screen and (min-width: 1200px) {
+        @media only screen and (min-width: 980px) {
           .item :global(img),
           .item :global(video) {
-            max-height: 220px;
+            max-height: 140px;
+          }
+        @media only screen and (min-width: 1280px) {
+          .item :global(img),
+          .item :global(video) {
+            max-height: 180px;
           }
         }
       `}</style>
