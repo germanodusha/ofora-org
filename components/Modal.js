67668cd8139bb68123857732526c137e288a0c7d
Mike Lima <maxsuelslima@gmail.com>
2022-06-25 20:13:49 -0300

    Some changes on style.

--- components/Modal.js
+++ components/Modal.js
@@ -5,32 +5,24 @@ const Modal = ({children, visible, onClose}) =>{
     console.log(children.props.children[1].props.children)
     if(visible){
         return(
-            <div className='container'>
-                <div className='backdrop' onClick={()=>onClose(null)}/>
-
+            <div className='container' >
                 <div className='modalContent' onClick={()=>console.log('content')}>
-                    <div>
-                    <span className='title'>{children.props.children[1].props.children}</span>
-                    <Image 
-                    alt={children.props.children[1].props.children} 
-                    src={children.props.children[0].props.src}
-                    layout='fill' 
-                    objectFit='contain' 
-                    />
+                    <div className='content'>
+                        <span className='title'>{children.props.children[1].props.children}</span>
+                        <div className='cover'>
+                            <Image 
+                            alt={children.props.children[1].props.children} 
+                            src={children.props.children[0].props.src}
+                            layout='fill' 
+                            objectFit='contain' 
+                            />
+                        </div>
                     </div>
                     <div className='closeIcon' onClick={()=>onClose(null)}>
-                        X
+                        &times;
                     </div>
                 </div>
                 <style jsx>{`
-                    .backdrop{
-                        position: fixed;
-                        top:0;
-                        left: 0;
-                        width: 100%;
-                        height: 100%;
-                        background: white;
-                    }
                     .container{
                         all:unset;
                         position: fixed;
@@ -38,27 +30,45 @@ const Modal = ({children, visible, onClose}) =>{
                         height: 100vh;
                         top:0;
                         left: 0;
-                        background: white;
-                        z-index:3;
+                        background: var(--yellow);
+                        z-index:1;
+                    }
+                    .cover{
+                        position: relative;
+                        top:0;
+                        left: 0;
+                        height: 600px;
+                        width: 600px;
+                    }
+                    .title{
+                        width: 100%;
+                        display: flex;
+                        align-items:center;
+                        justify-content:center;
                     }
                     .modalContent{
                         position: relative;
                         top: 92px;
                         left: 50%;
                         transform: translate(-50%, 0);
-                        width: 1000px;
-                        height: 700px;
-                        background: gray;
                         display: flex;
                         justify-content:center;
+                        align-items:center;
                         padding:10px;
                         z-index:1000
                     }
                     .closeIcon{
-                        position: relative;
+                        
                         right: -40%;
                         top: 0;
                         width: 1%;
+                        cursor: pointer;
+                        height: 0px;
+                        font-size:32px;
+                        align-self:flex-start;
+                    }
+                    .closeIcon:hover{
+                        color:var(--yellow)
                     }
                 `}</style>
             </div>
