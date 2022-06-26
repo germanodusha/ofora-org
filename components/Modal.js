63d3d34efd36effabfaced92de24d52eccab4cee
Diogo Vianna V. Araujo <diogovaraujo@gmail.com>
2022-06-25 21:33:15 -0300

    Transition

--- components/Modal.js
+++ components/Modal.js
@@ -1,73 +1,62 @@
 import Image from "next/image";
 
 const Modal = ({ title, media, visible, onClose }) => {
-  if (!visible) return null;
   return (
-    <div className="container">
-      <div className="modalContent">
-        <div className="content">
-          <span className="title">
-            {title}
-          </span>
-          <div className="cover">
+    <div className="backdrop">
+      <div className="content">
+        <span className="title">{title}</span>
+        <div className="cover">
+          {media.kind === "image" ? (
             <Image
               alt={title}
               src={media.url}
               layout="fill"
               objectFit="contain"
             />
-          </div>
-        </div>
-        <div className="closeIcon" onClick={() => onClose(null)}>
-          &times;
+          ) : (
+            <video autoPlay playsInline muted>
+              <source src={media.url} type="video/mp4" />
+            </video>
+          )}
         </div>
       </div>
+      <div className="close" onClick={() => onClose(null)}>
+        &times;
+      </div>
       <style jsx>{`
-        .container{
-            all:unset;
+        .backdrop {
             position: fixed;
-            width: 100vw;
-            height: 100vh;
-            top:0;
+            bottom: 0;
+            right: 0;
+            top: 0;
             left: 0;
             background: var(--yellow);
-            z-index:1;
+            z-index: 1;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            pointer-events: ${visible ? "all" : "none"};
+            opacity: ${visible ? 1 : 0};
+            transition: 0.6s opacity 0.1s;
         }
         .cover{
             position: relative;
             top:0;
             left: 0;
-            height: 50vh;
-            width: 40vw;
+            height: 75vh;
+            width: 75vw;
         }
-        .title{
-            width: 100%;
-            display: flex;
-            align-items:center;
-            justify-content:center;
+        .title {
+            display: block;
+            text-align: center;
         }
-        .modalContent{
-            position: relative;
-            top: 92px;
-            left: 50%;
-            transform: translate(-50%, 0);
-            display: flex;
-            justify-content:center;
-            align-items:center;
-            padding:10px;
-            z-index:1000
-            flex-shrink:0;
+        .close {
+            position: absolute;
+            top: 10vw;
+            right: 10vw;
+            background: red;
         }
-        .closeIcon{
-            all:unset;
-            width: 1%;
-            cursor: pointer;
-            height: 1px;
-            font-size:32px;
-            padding-left: 30px;
-            align-self:flex-start;
-        }
-        .closeIcon:hover{
+        .close:hover{
             color:var(--yellow)
         }
      `}</style>
