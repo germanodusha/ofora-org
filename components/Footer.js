273251d6ff17db5406d9d2f73d0dbc8a8a88daba
Mike Lima <maxsuelslima@gmail.com>
2022-06-27 23:11:42 -0300

    Backdrop added.

--- components/Footer.js
+++ components/Footer.js
@@ -10,8 +10,9 @@ export const Footer = () => {
 
   return (
     <footer className="flex py-3 pt-14 font-bold uppercase">
-      <div className="flex w-1/2 justify-center">
-        <Link href={"/what"}>{navigation.data.what}</Link>
+      <div className="flex w-1/2 justify-center whatIsButton">
+        <section><Link href={"/what"}>{navigation.data.what}</Link></section>
+        <div className="backdrop"/>
       </div>
       <nav className="flex w-1/2 justify-center">
         <ul className="flex flex-wrap gap-6 md:gap-10">
@@ -35,6 +36,31 @@ export const Footer = () => {
       <style jsx>{`
         li:hover :global(a) {
           color: #e8ff00;
+        }
+        .whatIsButton{
+          border: 1px transparent solid;
+          opacity: 1.5;
+        }
+        .backdrop{
+          position: fixed;
+          height: 100vh;
+          width: 100vw;
+          top: 0;
+          right: 0;
+          background: black;
+          opacity: 0;
+          pointer-events:none;
+          z-index:0;
+        }
+        .whatIsButton:hover>section{
+          background: linear-gradient(180deg, transparent 50%, white 50%);
+          opacity: 1;
+          z-index:2;
+          padding: 0px 10px;
+        }
+        .whatIsButton:hover>div{
+          background-color: black;
+          opacity: 0.5;
         }
       `}</style>
     </footer>
