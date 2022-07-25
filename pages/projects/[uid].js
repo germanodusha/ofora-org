b490339f877aecd12d543d2b3babd1daa5312a9f
maxsuel lima <maxsuelslima@gmail.com>
2022-07-24 23:50:26 -0300

    Safari test.

--- pages/projects/[uid].js
+++ pages/projects/[uid].js
@@ -198,7 +198,7 @@ const Project = ({ project }) => {
           left:0;
           width:100%;
           height:100px;
-          background:transparent;
+          background:white;
           background-image:linear-gradient(
             0deg,
             #fff 40%,
