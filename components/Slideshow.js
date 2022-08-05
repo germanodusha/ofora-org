000e7dc8823d6430bde7c0b6f5c4cbee85ed5850
Diogo Vianna V. Araujo <diogovaraujo@gmail.com>
2022-08-04 21:22:14 -0300

    Slideshow arrows

--- components/Slideshow.js
+++ components/Slideshow.js
@@ -6,13 +6,19 @@ const HEIGHT_FACTOR = 0.5;
 
 const Slideshow = ({ items }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isNavigating, setIsNavigating] = useState(false);
   const [slideshowRef, slideshowSize] = useElementSize(null);
   const windowSize = useWindowSize();
 
   const next = () => setCurrentIndex((currentIndex + 1) % items.length);
   const prev = () => setCurrentIndex((currentIndex - 1) % items.length);
+  const onNavMouseEnter = () => setIsNavigating(true);
+  const onNavMouseLeave = () => setIsNavigating(false);
 
-  useInterval(next, 6000);
+  useInterval(() => {
+    if (isNavigating) return;
+    next();
+  }, 6000);
 
   return (
     <>
@@ -35,7 +41,11 @@ const Slideshow = ({ items }) => {
             </div>
           ))}
         </div>
-        <div className="slideshow-nav">
+        <div
+          className="slideshow-nav"
+          onMouseEnter={onNavMouseEnter}
+          onMouseLeave={onNavMouseLeave}
+        >
           <div onClick={prev}>{"←"}</div>
           <div onClick={next}>{"→"}</div>
         </div>
@@ -70,12 +80,12 @@ const Slideshow = ({ items }) => {
           align-items: center;
           padding: 20px;
           opacity: 0;
-          transition: opacity .5s ease-in-out;
+          transition: opacity 0.5s ease-in-out;
           transition-delay: 0s;
         }
         .slideshow-item.active {
           opacity: 1;
-          transition-delay: .6s;
+          transition-delay: 0.6s;
         }
         .slideshow-nav {
           position: absolute;
@@ -87,8 +97,15 @@ const Slideshow = ({ items }) => {
           font-size: 1.625rem;
         }
         .slideshow-nav div {
-          cursor: pointer;
           padding: 0 10px;
+        }
+        .slideshow-nav div:first-child {
+          opacity: ${currentIndex === 0 ? 0 : 1};
+          pointer-events: ${currentIndex === 0 ? "none" : "auto"};
+        }
+        .slideshow-nav div:last-child {
+          opacity: ${currentIndex === items.length - 1 ? 0 : 1};
+          pointer-events: ${currentIndex === items.length - 1 ? "none" : "auto"};
         }
         .slideshow-nav div:hover {
           color: var(--yellow);
