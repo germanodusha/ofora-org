f97c22672172955886480641b7cb9b63a8d4bc8d
Diogo Vianna V. Araujo <diogovaraujo@gmail.com>
2022-08-10 23:49:31 -0300

    Footer refactoring.

--- components/Footer.js
+++ components/Footer.js
@@ -1,20 +1,17 @@
 import Link from "next/link";
-import { PrismicLink } from "@prismicio/react";
-import { linkResolver } from "../prismicio";
 import { useContext } from "react";
 import { Context } from "../pages/_app";
 import { useRouter } from "next/router";
 import Highlighted from "./Highlighted";
 
 export const Footer = () => {
-  const { page, navigation } = useContext(Context);
-  const languages = page ? [page.lang, ...page.alternate_languages] : [];
+  const { navigation } = useContext(Context);
   const { asPath } = useRouter();
   if (asPath === "/what") {
     return <div></div>;
   }
   const highlightedColor = asPath.includes("/projects/") ? "yellow" : "white";
-  console.log(highlightedColor);
+
   return (
     <>
       <footer className="flex py-3 pt-14 font-bold uppercase">
@@ -30,31 +27,17 @@ export const Footer = () => {
             <li>
               <Highlighted color={highlightedColor}>
                 <a href={"/pt-br"}>
-                  <a>PT</a>
+                  PT
                 </a>
               </Highlighted>
             </li>
             <li>
               <Highlighted color={highlightedColor}>
                 <a href="/en-us">
-                  <a>EN</a>
+                  EN
                 </a>
               </Highlighted>
             </li>
-            {/* {languages.map((lang) => (
-            <li key={lang.lang}>
-              <Highlighted color={highlightedColor}>
-                <PrismicLink href={linkResolver(lang)} locale={lang.lang}>
-                  <span className="sr-only" style={{ fontWeight: "normal" }}>
-                    {lang.lang}
-                  </span>
-                  <a style={{ fontWeight: "normal" }}>
-                    {lang.lang.split("-")[0]}
-                  </a>
-                </PrismicLink>
-              </Highlighted>
-            </li>
-          ))} */}
           </ul>
         </nav>
         <style jsx>{`
