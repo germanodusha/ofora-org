a87491c3d8d4c37cfe5e226dc7f83826f226bf2f
Diogo Vianna V. Araujo <diogovaraujo@gmail.com>
2022-08-15 22:59:46 -0300

    Fixed header link

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,12 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { useContext } from "react";
 import { Context } from "../pages/_app";
 import Highlighted from "./Highlighted";
 
 export const Header = () => {
   const { navigation } = useContext(Context);
+  const { locale } = useRouter()
 
   return (
     <header className="header-root flex flex-wrap py-3 pb-14">
@@ -12,9 +14,7 @@ export const Header = () => {
         <ul className="header-content flex w-full flex-wrap justify-around text-center font-semibold uppercase">
           <li className="">
             <Highlighted>
-              <Link href={"/"}>
-                <a>{navigation?.data?.home}</a>
-              </Link>
+                <a href={"/"+locale}>{navigation?.data?.home}</a>
             </Highlighted>
           </li>
           <li className="">
